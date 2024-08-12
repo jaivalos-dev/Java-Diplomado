1845b3979ad4670cc1e57fb0fe0788f7cc543d41
Javier PC <jaivalos.dev@gmail.com>
2024-08-11 21:59:20 -0600

    Dia 1, Hola mundo. Pag: 15. GD1

---
+++ Semana 1/1-HolaMundo/src/Paquete/HolaMundo.java
@@ -0,0 +1,10 @@
+package Paquete;
+
+public class HolaMundo {
+    public static void main(String args[]){
+        System.out.println("Hola mundo");
+
+        String nombre = "Javier";
+        System.out.println("Mi nombre es: " + nombre);
+    }
+}
